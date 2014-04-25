0a51a3980e64bf7707b2f1235000533c73fbc804
Demian Neidetcher <demian0311@gmail.com>
2014-04-25 12:13:38 -0500

    remove lib

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,8 +48,9 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.27'
+        runtime 'mysql:mysql-connector-java:5.1.27'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
+        build 'com.google.code.simple-spring-memcached:spymemcached:2.8.1'
     }
 
     plugins {
